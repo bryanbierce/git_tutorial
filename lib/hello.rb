c23495fea69b323f3cc96cdcf330b090f1f3d473
Bryan Bierce <bryanbierce@gmail.com>
2015-10-03 15:27:37 -0700

    Hello uses greeter

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,4 +1,9 @@
+require 'greeter'
+
 #Default is "World"
 #Author: Bryan Bierce (bryanbierce@gmail.com)
 name = ARGV.first || "World"
 puts "Hello #{name}!"
+
+greeter = Greeter.new(name)
+puts greeter.greet
